c8f5e61956459b42bb8ce34dd3b138278edace7b
srodriguezd <sara-in-blue_2@hotmail.com>
2015-10-26 15:40:57 +0100

    Update Triángulo.java

--- src/m3_trimestre01/Triángulo.java
+++ src/m3_trimestre01/Triángulo.java
@@ -9,63 +9,55 @@ package m3_trimestre01;
  *
  * @author sara
  */
-public class Triángulo {
-    // Declaración de atributos 
-   private double base;
-   private double altura;
-   private Punto origen;
-   
-   // Constructor 
-   public Triángulo(double base, double altura, Punto origen)
-    { 
-       this.base = base; 
-       this.altura = altura; 
-       this.origen = origen;
-    } 
+public class Triangulo2 extends Figura2 implements MetodosFigura2{
+    private double base;
+    private double altura;
 
-// Definición de métodos  
+    public Triangulo2(double base, double altura, Punto2 origen) {
+        super(origen);
+        this.base = base;
+        this.altura = altura;
+    }
 
     public double getBase() {
         return base;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-    
-    public Punto getOrigen() {
-        return origen;
     }
 
     public void setBase(double base) {
         this.base = base;
     }
 
+    public double getAltura() {
+        return altura;
+    }
+
     public void setAltura(double altura) {
         this.altura = altura;
     }
-
-    public void setOrigen(Punto origen) {
-        this.origen = origen;
-    }
-
-    public double Area() 
-   { 
+    
+    @Override
+    public double Area() {
         double resultado = (this.base*this.altura/2);
         return resultado;
-   }  
-    
-    public double Perímetro() 
-   { 
+    }
+
+    @Override
+    public double Perímetro() {
         double resultado = (this.base * 3 ); //Esto si fuera equilátero...
         return resultado;
-   }  
+    }
+
+    @Override
+    public void Escalar(double n) {
+        this.setBase((this.base) * (n/100));
+        this.setAltura((this.altura) * (n/100));
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo2{" + "base=" + base + ", altura=" + altura + '}';
+    }
     
-    public void Escalar(double r) 
-   { 
-        this.setBase((this.base) * (r/100));
-        this.setAltura((this.altura) * (r/100));
-        
-   } 
+    
     
 }
