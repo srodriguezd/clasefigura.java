185e2b072593e0b320b7b9439a34c9b3c79286d6
srodriguezd <sara-in-blue_2@hotmail.com>
2015-10-26 15:32:47 +0100

    Update Círculo.java

--- src/m3_trimestre01/Círculo.java
+++ src/m3_trimestre01/Círculo.java
@@ -9,54 +9,45 @@ package m3_trimestre01;
  *
  * @author sara
  */
-public class Círculo {
-    // Declaración de atributos 
-   private int radio;
-   
-   // Constructor 
-   public Círculo(int radio, Punto origen) 
-    { 
-       super.Figura(origen);
-       this.radio=radio;
-    } 
-   
-// Definición de métodos  
+public class Circulo2 extends Figura2 implements MetodosFigura2 {
+
+   private double radio;
+
+    public Circulo2(double radio, Punto2 origen) {
+        super(origen);
+        this.radio = radio;
+    }
 
     public double getRadio() {
         return radio;
     }
 
-    public Punto getOrigen() {
-        return origen;
-    }
-
     public void setRadio(double radio) {
         this.radio = radio;
     }
-
-    public void setOrigen(Punto origen) {
-        this.origen = origen;
-    }
-
-    public double Area() 
-   { 
+    
+    @Override
+    public double Area() {
         double resultado = 3.1416 * Math.pow(this.radio,2);
         return resultado;
-   }  
-    
-    public double Perímetro() 
-   { 
+    }
+
+    @Override
+    public double Perímetro() {
         double resultado = 2*3.1416*this.radio;
         return resultado;
-   }  
+    }
+
+    @Override
+    public void Escalar(double n) {
+        this.setRadio((this.radio + this.radio) * (n/100));
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo2{" + "radio=" + radio + '}';
+    }
     
-    public void Escalar(double r) 
-   { 
-        this.setRadio((this.radio + this.radio) * (r/100));
-        
-   } 
-    
-  
+
     
 }
-
